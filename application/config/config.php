dfc2254973008690d8a35ad04b160f5c83d38f27
Winner <winnerjohnudaundo94>
2023-07-27 17:36:46 +0800

    with login

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.68.108/7-11';
+$config['base_url'] = 'http://192.168.80.31/7-ELEVEN';
 
 /*
 |--------------------------------------------------------------------------
--- index.php
+++ index.php
@@ -54,6 +54,7 @@
  * NOTE: If you change these, also change the error_reporting() code below
  */
 	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+	define('IMAGE_URL', '/7-ELEVEN/assets/images/');
 
 /*
  *---------------------------------------------------------------
